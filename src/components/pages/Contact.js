c6242978ef772382ae818480c320829c524bca11
unknown <scarletfedora@gmail.com>
2022-07-18 13:12:02 -0700

    Working on prject component to load in project cards, and worked on contact form to handle forms

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,12 +1,86 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { validateEmail } from '../../utils/helpers';
 
 export default function Contact() {
+
+    const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
+    const [message, setMessage] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const handleInputChange = (e) => {
+
+        const { target } = e;
+        const inputType = target.name;
+        const inputValue = target.value;
+
+        if (inputType === 'email') {
+            setEmail(inputValue);
+        } else if (inputType === 'name') {
+            setName(inputValue);
+        } else {
+            setMessage(inputValue);
+        }
+    };
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+
+        if(!validateEmail(email)) {
+            setErrorMessage('Please enter a valid email');
+            return;
+        }
+        if(!name) {
+            setErrorMessage('Please leave a name');
+            return;
+        }
+        if(!message) {
+            setErrorMessage('Please leave a response');
+            return;
+        }
+        alert(`Thank you, ${name}!`)
+
+        setEmail('');
+        setName('');
+        setMessage('');
+    };
+
     return (
         <div>
             <h1>Contact Me...</h1>
             <p>
                 This is where I will be linking my contact form.
             </p>
+            <form>
+                <input
+                    value={name}
+                    name="name"
+                    onChange={handleInputChange}
+                    type="text"
+                    placeholder="name"
+                />
+                <input
+                    value={email}
+                    name="email"
+                    onChange={handleInputChange}
+                    type="text"
+                    placeholder="email"
+                />
+                <input
+                    value={message}
+                    name="text"
+                    onChange={handleInputChange}
+                    type="text"
+                    placeholder="message"
+                />
+                <button type='button' onClick={handleFormSubmit}>Submit</button>
+            </form>
+            {errorMessage && (
+                <div>
+                    <p>{errorMessage}</p>
+                </div>
+            )}
         </div>
     );
 }
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,12 +1,35 @@
 import React from 'react';
+import Project from '../Project';
 
-export default function Portfolio() {
+const projects = [
+    {
+        id: 1,
+        name: "Project 1",
+        description: "This is a description!",
+        image: require('../assets/head.png'),
+    },
+    {
+        id: 2,
+        name: "Project 2",
+        description: "This is a description!",
+        image: require('../assets/head.png'),
+    },
+    {
+        id: 3,
+        name: "Project 3",
+        description: "This is a description!",
+        image: require('../assets/head.png'),
+    },
+]
+
+export default function Portfolio(props) {
     return (
         <div>
             <h1>My Projects</h1>
             <p>
                 This is where I wil be hosting my projects.
             </p>
+            <Project projects={projects} />;
         </div>
     );
 }
